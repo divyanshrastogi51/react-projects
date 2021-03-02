fceeb12223b9fd06c7f982fe5d2cd7c6fdeeae4d
divyanshrastogi51 <divyanshrastogi501@gmail.com>
2021-03-02 21:16:38 +0530

    project done and deployed

--- 4-accordion-qna/src/App.js
+++ 4-accordion-qna/src/App.js
@@ -4,18 +4,21 @@ import SingleQuestion from './Question';
 function App() {
   const [questions, setQuestions] = useState(data);
   return (
-    <main>
-      <div className='container'>
-        <h3>questions and answers about login</h3>
-        <section className='info'>
-          {questions.map((question) => {
-            return (
-              <SingleQuestion key={question.id} {...question}></SingleQuestion>
-            );
-          })}
-        </section>
-      </div>
-    </main>
+    <>
+      <main>
+        <div className='container'>
+          <h3>questions and answers about login</h3>
+          <section className='info'>
+            {questions.map((question) => {
+              return (
+                <SingleQuestion key={question.id} {...question}></SingleQuestion>
+              );
+            })}
+          </section>
+        </div>
+      </main>
+      <footer className='footer'>Made with ❤️ by  <a className='link' href="https://divyansh-rastogi.netlify.app/"> Divyansh Rastogi </a>  Inspired from John Smilga</footer>
+    </>
   );
 }
 
