ffb6a672b3e046dbe421ed1d51f605f89fce9c05
divyanshrastogi51 <divyanshrastogi501@gmail.com>
2021-03-06 21:13:33 +0530

    project deployed'

--- 7-slider/src/App.js
+++ 7-slider/src/App.js
@@ -4,7 +4,7 @@ import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
-  const [index, setIndex] = React.useState(0);
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const lastIndex = people.length - 1;
@@ -26,45 +26,48 @@ function App() {
   }, [index]);
 
   return (
-    <section className="section">
-      <div className="title">
-        <h2>
-          <span>/</span>reviews
+    <>
+      <section className="section">
+        <div className="title">
+          <h2>
+            <span>/</span>reviews
         </h2>
-      </div>
-      <div className="section-center">
-        {people.map((person, personIndex) => {
-          const { id, image, name, title, quote } = person;
+        </div>
+        <div className="section-center">
+          {people.map((person, personIndex) => {
+            const { id, image, name, title, quote } = person;
 
-          let position = 'nextSlide';
-          if (personIndex === index) {
-            position = 'activeSlide';
-          }
-          if (
-            personIndex === index - 1 ||
-            (index === 0 && personIndex === people.length - 1)
-          ) {
-            position = 'lastSlide';
-          }
+            let position = 'nextSlide';
+            if (personIndex === index) {
+              position = 'activeSlide';
+            }
+            if (
+              personIndex === index - 1 ||
+              (index === 0 && personIndex === people.length - 1)
+            ) {
+              position = 'lastSlide';
+            }
 
-          return (
-            <article className={position} key={id}>
-              <img src={image} alt={name} className="person-img" />
-              <h4>{name}</h4>
-              <p className="title">{title}</p>
-              <p className="text">{quote}</p>
-              <FaQuoteRight className="icon" />
-            </article>
-          );
-        })}
-        <button className="prev" onClick={() => setIndex(index - 1)}>
-          <FiChevronLeft />
-        </button>
-        <button className="next" onClick={() => setIndex(index + 1)}>
-          <FiChevronRight />
-        </button>
-      </div>
-    </section>
+            return (
+              <article className={position} key={id}>
+                <img src={image} alt={name} className="person-img" />
+                <h4>{name}</h4>
+                <p className="title">{title}</p>
+                <p className="text">{quote}</p>
+                <FaQuoteRight className="icon" />
+              </article>
+            );
+          })}
+          <button className="prev" onClick={() => setIndex(index - 1)}>
+            <FiChevronLeft />
+          </button>
+          <button className="next" onClick={() => setIndex(index + 1)}>
+            <FiChevronRight />
+          </button>
+        </div>
+      </section>
+      <footer className='footer'>Made with ❤️ by  <a className='link' href="https://divyansh-rastogi.netlify.app/"> Divyansh Rastogi </a>  Inspired from John Smilga</footer>
+    </>
   );
 }
 
