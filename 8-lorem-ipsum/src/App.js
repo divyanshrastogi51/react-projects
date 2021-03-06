c021f1db03d06b0bd42f1ddefc745a86c3a5dc70
divyanshrastogi51 <divyanshrastogi501@gmail.com>
2021-03-06 21:42:02 +0530

    project deployed'

--- 8-lorem-ipsum/src/App.js
+++ 8-lorem-ipsum/src/App.js
@@ -16,25 +16,28 @@ function App() {
     setText(data.slice(0, amount));
   };
   return (
-    <section className='section-center'>
-      <h3>tired of boring lorem ipsum?</h3>
-      <form className='lorem-form' onSubmit={handleSubmit}>
-        <label htmlFor='amount'>paragraphs:</label>
-        <input
-          type='number'
-          name='amount'
-          id='amount'
-          value={count}
-          onChange={(e) => setCount(e.target.value)}
-        />
-        <button className='btn'>generate</button>
-      </form>
-      <article className='lorem-text'>
-        {text.map((item, index) => {
-          return <p key={index}>{item}</p>;
-        })}
-      </article>
-    </section>
+    <>
+      <section className='section-center'>
+        <h3>tired of boring lorem ipsum?</h3>
+        <p>Minimum 1 and Maximum 8</p>
+        <form className='lorem-form' onSubmit={handleSubmit}>
+          <label htmlFor='amount'>paragraphs:</label>
+          <input
+            type='number'
+            name='amount'
+            id='amount'
+            value={count}
+            onChange={(e) => setCount(e.target.value)}
+          />
+          <button className='btn'>generate</button>
+        </form>
+        <article className='lorem-text'>
+          {text.map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })}
+        </article>
+      </section>
+    </>
   );
 }
 
