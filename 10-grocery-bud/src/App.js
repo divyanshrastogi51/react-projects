ec9601b491d91de18a7b3487192256dc7e12437c
divyanshrastogi51 <divyanshrastogi501@gmail.com>
2021-03-09 22:14:04 +0530

    changes done

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -62,33 +62,36 @@ function App() {
     localStorage.setItem('list', JSON.stringify(list));
   }, [list]);
   return (
-    <section className='section-center'>
-      <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+    <>
+      <section className='section-center'>
+        <form className='grocery-form' onSubmit={handleSubmit}>
+          {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
 
-        <h3>grocery bud</h3>
-        <div className='form-control'>
-          <input
-            type='text'
-            className='grocery'
-            placeholder='e.g. eggs'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <button type='submit' className='submit-btn'>
-            {isEditing ? 'edit' : 'submit'}
+          <h3>grocery bud</h3>
+          <div className='form-control'>
+            <input
+              type='text'
+              className='grocery'
+              placeholder='e.g. eggs'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button type='submit' className='submit-btn'>
+              {isEditing ? 'edit' : 'submit'}
+            </button>
+          </div>
+        </form>
+        {list.length > 0 && (
+          <div className='grocery-container'>
+            <List items={list} removeItem={removeItem} editItem={editItem} />
+            <button className='clear-btn' onClick={clearList}>
+              clear items
           </button>
-        </div>
-      </form>
-      {list.length > 0 && (
-        <div className='grocery-container'>
-          <List items={list} removeItem={removeItem} editItem={editItem} />
-          <button className='clear-btn' onClick={clearList}>
-            clear items
-          </button>
-        </div>
-      )}
-    </section>
+          </div>
+        )}
+      </section>
+      <footer className='footer'>Made with ❤️ by  <a className='link' href="https://divyansh-rastogi.netlify.app/"> Divyansh Rastogi </a>  Inspired from John Smilga</footer>
+    </>
   );
 }
 
